550d2201fb59ebd26dcfd2c9d571dcd6850f6245
maakhady <makhadypro@gmail.com>
2025-01-22 23:40:43 +0000

    feat: configuration pour render avec port dynamique

--- pointage-back/docker/start.sh
+++ pointage-back/docker/start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# Remplacer la variable PORT dans la configuration nginx
+envsubst '${PORT}' < /etc/nginx/conf.d/app.conf > /etc/nginx/conf.d/default.conf
+
+# Démarrer Nginx
 service nginx start
+
+# Démarrer PHP-FPM en premier plan
 php-fpm
